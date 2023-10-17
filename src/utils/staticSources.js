594321fe0169cf060cc63b4e06ea0b97924ebc9f
Akash Gudi <akashgudi@gmail.com>
2023-10-17 20:10:37 +0530

    mapped images to items

---
+++ src/utils/staticSources.js
@@ -0,0 +1,30 @@
+import alooMutter from '../assets/static/aloo-mutter.jpg'
+import bhindi from '../assets/static/bhindi.jpg'
+import dalRice from '../assets/static/dal-rice.jpg'
+import chicken from '../assets/static/chicken.jpg'
+import channa from '../assets/static/channa.jpg'
+import egg from '../assets/static/egg.jpg'
+import friedRice from '../assets/static/fried-rice.jpg'
+import gobi from '../assets/static/gobi.jpg'
+import noodles from '../assets/static/noodles.jpg'
+import paneer from '../assets/static/paneer.jpg'
+import pasta from '../assets/static/pasta.jpg'
+import rajma from '../assets/static/rajma.jpg'
+
+export const sourceDict = {
+    "aloo_mutter": alooMutter,
+    "bhindi": bhindi,
+    "dal_rice": dalRice,
+    "chicken": chicken,
+    "channa": channa,
+    "egg": egg,
+    "fried_rice": friedRice,
+    "gobi": gobi,
+    "noodles": noodles,
+    "paneer": paneer,
+    "pasta": pasta,
+    "rajma": rajma
+
+}
+
+console.log(sourceDict)
