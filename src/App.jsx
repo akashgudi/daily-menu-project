8fdcfa8be3dec3cc4ab258e7a4d90f121416fff7
Akash Gudi <akashgudi@gmail.com>
2023-10-17 16:27:41 +0530

    added json data

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import proteins from './data/proteins.json'
+import regular from './data/regular.json'
 function App() {
 
   return (
     <>
+      {JSON.stringify(proteins)}
+      {JSON.stringify(regular)}
 
     </>
   )
