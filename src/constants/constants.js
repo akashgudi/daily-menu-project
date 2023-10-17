78f3518711dc201d9c1e4ce09eee29ef8f12f00e
Akash Gudi <akashgudi@gmail.com>
2023-10-17 18:07:36 +0530

    added data required

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,5 @@
+import proteins from '../data/proteins.json'
 export const COOK_DAYS_IN_WEEK = 6
 export const COOK_MEALS_PER_DAY = 2
 export const NUM_OF_MEALS_PER_WEEK = COOK_DAYS_IN_WEEK * COOK_MEALS_PER_DAY  
+export const NUM_OF_PROTEINS = proteins.length
