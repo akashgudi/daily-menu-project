767657867db003f994e95dc5325b86d47c5e9749
Akash Gudi <akashgudi@gmail.com>
2023-10-17 18:07:22 +0530

    added app files

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 //import data
 import proteins from './data/proteins.json'
 import regular from './data/regular.json'
-
 //import consts 
-import {} from './constants/constants'
-
-
+import { } from './constants/constants'
 //import utils
 import { generateMenu } from './utils/generateMenu'
 import { shuffle } from './utils/shuffle'
+import { ValidateMenu } from './utils/validateMenu'
+import { getWeek } from './utils/getWeek'
+//import components
+import { ItemCard } from './components/itemCard/ItemCard'
+
 function App() {
+
+  const [menu, setMenu] = useState(ValidateMenu(shuffle(generateMenu(proteins, regular))))
+  
+
+  const shuffleMenu = () => {
+    setMenu(shuffle((generateMenu(proteins, regular))))
+  }
 
   return (
     <>
-      {JSON.stringify(shuffle((generateMenu(proteins,regular))))}
+      <button onClick={shuffleMenu}>Shuffle</button>
+      {menu.map((item, idx) => <ItemCard item={item} key={idx} />)}
     </>
   )
 }
